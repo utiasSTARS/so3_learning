bf64bd8d40d437e8cdebc4a963bd453b35177b50
Valentin Peretroukhin <valentinp@gmail.com>
2019-02-20 17:05:55 -0500

    Changing to depth

--- run_7scenes_experiment.py
+++ run_7scenes_experiment.py
@@ -78,7 +78,8 @@ if __name__ == '__main__':
     ])
 
     transform_depth = transforms.Compose([
-        transforms.Resize(224)
+        transforms.Resize(224),
+        transforms.ToTensor()
     ])
 
     train_loader = DataLoader(SevenScenesData(args.scene, '/home/valentinp/research/data/7scenes', train=True, transform=transform_depth),
