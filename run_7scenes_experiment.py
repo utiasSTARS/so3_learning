60dac19343cb669903911fec23767c18bd16a879
Valentin Peretroukhin <valentinp@gmail.com>
2019-02-19 21:50:23 -0500

    Added jitter back

--- run_7scenes_experiment.py
+++ run_7scenes_experiment.py
@@ -49,7 +49,8 @@ if __name__ == '__main__':
 
     num_hydra_heads=args.num_heads
     model = QuaternionCNN(num_hydra_heads=num_hydra_heads)
-    model.sensor_net.freeze_layers()
+    #model.sensor_net.freeze_layers()
+
     model.to(dtype=tensor_type, device=device)
 
 
@@ -69,7 +70,7 @@ if __name__ == '__main__':
     transform_jitter = transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
-        transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.4),
+        transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.45, hue=0.45),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225])
