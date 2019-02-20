787dc23d61a67f1b7fe43855b7a05051253f5929
Valentin Peretroukhin <valentinp@gmail.com>
2019-02-19 20:36:20 -0500

    Changed back

--- run_7scenes_experiment.py
+++ run_7scenes_experiment.py
@@ -68,7 +68,7 @@ if __name__ == '__main__':
     transform_jitter = transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
-        transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.3),
+        transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.4),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225])
