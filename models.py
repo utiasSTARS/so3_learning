f66391988f68514124c44b241465c12c5bc5ea72
Valentin Peretroukhin <valentinp@gmail.com>
2019-02-20 17:07:57 -0500

    Changing to depth

--- models.py
+++ models.py
@@ -98,15 +98,14 @@ class BasicCNN(torch.nn.Module):
             # StandardBlock(D_in_sensor, self.sensor_net_dim),
             conv_unit(1, 64, kernel_size=3, stride=2, padding=1),
             conv_unit(64, 64, kernel_size=3, stride=2, padding=1),
-            conv_unit(64, 128, kernel_size=3, stride=2, padding=1),
-            conv_unit(128, 128, kernel_size=3, stride=2, padding=1),
-            conv_unit(128, 3, kernel_size=3, stride=2, padding=1),
+            conv_unit(64, 128, kernel_size=3, stride=2, padding=1)
         )
         self.fc = torch.nn.Linear(3*7*7, feature_dim)
 
 
     def forward(self, x):
         out = self.cnn0(x)
+        print(out.shape)
         out = out.view(out.shape[0], -1)
         out = self.fc(out)
         return out
