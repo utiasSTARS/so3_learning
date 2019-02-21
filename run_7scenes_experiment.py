7adad266d54404d1632ee1ad4607c9acee5047e9
Valentin Peretroukhin <valentinp@gmail.com>
2019-02-20 23:53:27 -0500

    Re-running 7scene experiments with proper checkpointing to cpu

--- run_7scenes_experiment.py
+++ run_7scenes_experiment.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', type=int, default=16)
     parser.add_argument('--epoch_display', type=int, default=1)
     parser.add_argument('--lr', type=float, default=1e-4)
-    parser.add_argument('--total_epochs', type=int, default=15)
+    parser.add_argument('--total_epochs', type=int, default=10)
     parser.add_argument('--num_heads', type=int, default=25)
     parser.add_argument('--scene', type=str, default='chess')
 
