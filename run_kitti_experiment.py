f7ba96fb156d89ef6e95ed873351bf3c195f67fa
Valentin Peretroukhin <valentinp@gmail.com>
2019-03-09 17:57:32 -0500

    Moving to no cropping, only resizing

--- run_kitti_experiment.py
+++ run_kitti_experiment.py
@@ -67,9 +67,16 @@ if __name__ == '__main__':
         lr=args.lr)
 
     #Load datasets
+    # transform = transforms.Compose([
+    #     transforms.Resize(224),
+    #     transforms.CenterCrop(224),
+    #     transforms.ToTensor(),
+    #     transforms.Normalize(mean=[0.485, 0.456, 0.406],
+    #                          std=[0.229, 0.224, 0.225])
+    # ])
+
     transform = transforms.Compose([
-        transforms.Resize(224),
-        transforms.CenterCrop(224),
+        transforms.Resize((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225])
