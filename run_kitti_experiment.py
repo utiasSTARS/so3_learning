66624adfb8e9b0e5101129f9a4ae8d4360b4e3b6
Valentin Peretroukhin <valentinp@gmail.com>
2019-03-12 14:18:28 -0400

    Fixed set quat sign bug

--- run_kitti_experiment.py
+++ run_kitti_experiment.py
@@ -1,8 +1,5 @@
 import numpy as np
 import torch
-from torch import autograd
-import os
-
 
 import matplotlib
 matplotlib.use('Agg')
@@ -80,7 +77,7 @@ if __name__ == '__main__':
 
     train_loader = DataLoader(KITTIVODataset(kitti_data_pickle_file, transform_img=transform, run_type='train'),
                         batch_size=args.batch_size, pin_memory=True,
-                        shuffle=True, num_workers=12, drop_last=False)
+                        shuffle=True, num_workers=12, drop_last=True)
     valid_loader = DataLoader(KITTIVODataset(kitti_data_pickle_file, transform_img=transform, run_type='test'),
                               batch_size=args.batch_size, pin_memory=True,
                               shuffle=False, num_workers=12, drop_last=False)
