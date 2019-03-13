4ad3de8ffc0f3c908b8ca42108712863cbc8dfb6
Valentin Peretroukhin <valentinp@gmail.com>
2019-03-13 18:07:46 -0400

    Switched dense to SE(3)

--- kitti/create_kitti_training_data_single_memory.py
+++ kitti/create_kitti_training_data_single_memory.py
@@ -110,8 +110,8 @@ def main():
     #all_trials = ['00', '02', '05', '06']
 
 
-    train_pose_deltas = [3] #How far apart should each quad image be? (KITTI is at 10hz, can input multiple)
-    test_pose_delta = 3
+    train_pose_deltas = [1,5] #How far apart should each quad image be? (KITTI is at 10hz, can input multiple)
+    test_pose_delta = 5
     add_reverse = False #Add reverse transformations
 
     #Where is the KITTI data?
