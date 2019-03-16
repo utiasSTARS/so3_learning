232ba0b005e2ade9d9ec5398e264e552e7ebbecf
Valentin Peretroukhin <valentinp@gmail.com>
2019-03-16 00:02:18 -0400

    Trying a q target sigma

--- run_kitti_experiment.py
+++ run_kitti_experiment.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     parser.add_argument('--lr', type=float, default=1e-4)
     parser.add_argument('--total_epochs', type=int, default=25)
     parser.add_argument('--num_heads', type=int, default=25)
-    parser.add_argument('--q_target_sigma', type=float, default=0.)
+    parser.add_argument('--q_target_sigma', type=float, default=0.1)
     parser.add_argument('--freeze_body', action='store_true', default=False)
 
     args = parser.parse_args()
--- run_kitti_exps.sh
+++ run_kitti_exps.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-python3 run_kitti_experiment.py --lr 1e-3 --seq 00 --total_epochs 25
-python3 run_kitti_experiment.py --lr 1e-3 --seq 02 --total_epochs 25
-python3 run_kitti_experiment.py --lr 1e-3 --seq 05 --total_epochs 25
+python3 run_kitti_experiment.py --lr 2e-3 --seq 00 --total_epochs 25
+python3 run_kitti_experiment.py --lr 2e-3 --seq 02 --total_epochs 25
+python3 run_kitti_experiment.py --lr 2e-3 --seq 05 --total_epochs 25
 
