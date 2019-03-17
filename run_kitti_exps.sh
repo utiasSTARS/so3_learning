d97672821137030f051d6ed41733a5f86c2f76ec
Valentin Peretroukhin <valentinp@gmail.com>
2019-03-17 17:36:44 -0400

    Reducing learning rate

--- run_kitti_exps.sh
+++ run_kitti_exps.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-python3 run_kitti_experiment.py --lr 5e-5 --seq 00 --total_epochs 30
-python3 run_kitti_experiment.py --lr 5e-5 --seq 02 --total_epochs 30
-python3 run_kitti_experiment.py --lr 5e-5 --seq 05 --total_epochs 30
+python3 run_kitti_experiment.py --lr 2e-5 --seq 00 --total_epochs 25
+python3 run_kitti_experiment.py --lr 2e-5 --seq 02 --total_epochs 25
+python3 run_kitti_experiment.py --lr 2e-5 --seq 05 --total_epochs 25
+python3 run_kitti_experiment.py --lr 2e-5 --seq 05 --total_epochs 25
 
