0f072c9ee2b69552213d1043ee7b0d4f1f79463c
Valentin Peretroukhin <valentinp@gmail.com>
2019-03-17 15:54:26 -0400

    Reformated for new runs with 120x400 images

--- run_kitti_exps.sh
+++ run_kitti_exps.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-python3 run_kitti_experiment.py --lr 5e-5 --seq 00 --total_epochs 40
-python3 run_kitti_experiment.py --lr 5e-5 --seq 02 --total_epochs 40
-python3 run_kitti_experiment.py --lr 5e-5 --seq 05 --total_epochs 40
+python3 run_kitti_experiment.py --lr 5e-5 --seq 00 --total_epochs 30
+python3 run_kitti_experiment.py --lr 5e-5 --seq 02 --total_epochs 30
+python3 run_kitti_experiment.py --lr 5e-5 --seq 05 --total_epochs 30
 
