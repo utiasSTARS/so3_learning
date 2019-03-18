735757ede200faa5e998c261af0ca61da9056550
Valentin Peretroukhin <valentinp@gmail.com>
2019-03-17 23:18:42 -0400

    Retraining with delta of 2

--- run_kitti_exps.sh
+++ run_kitti_exps.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-python3 run_kitti_experiment.py --lr 2e-5 --seq 00 --total_epochs 25
-python3 run_kitti_experiment.py --lr 2e-5 --seq 02 --total_epochs 25
-python3 run_kitti_experiment.py --lr 2e-5 --seq 05 --total_epochs 25
+python3 run_kitti_experiment.py --lr 3e-5 --seq 00 --total_epochs 25
+python3 run_kitti_experiment.py --lr 3e-5 --seq 02 --total_epochs 25
+python3 run_kitti_experiment.py --lr 3e-5 --seq 05 --total_epochs 25
 
