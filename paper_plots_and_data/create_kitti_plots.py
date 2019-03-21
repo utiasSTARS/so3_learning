2d5216938168e6d6c96d6d53048cea499db11ed3
Valentin Peretroukhin <valentinp@gmail.com>
2019-03-21 11:24:15 -0400

    KITTI plots

--- paper_plots_and_data/create_kitti_plots.py
+++ paper_plots_and_data/create_kitti_plots.py
@@ -13,6 +13,10 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import scipy.io as sio
 from utils import *
+from pyslam.metrics import TrajectoryMetrics
+from pyslam.visualizers import TrajectoryVisualizer
+import csv
+
 
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
@@ -124,14 +128,120 @@ def create_7scenes_abs_with_sigmas_plot(scene_checkpoint):
     fig.savefig(output_file, bbox_inches='tight')
     plt.close(fig)
 
-def create_kitti_seg_plots():
-    pass
+def create_kitti_topdown_plot(tm_svo, tm_fusion, seq):
+    tm_dict = {'viso2-s': tm_svo, 'Fusion': tm_fusion}
+    vis = TrajectoryVisualizer(tm_dict)
+    fig, ax = vis.plot_topdown(which_plane='xy')
+    output_file = 'kitti_{}_topdown.pdf'.format(seq)
+    fig.savefig(output_file, bbox_inches='tight')
 
-def output_kitti_stats(tm_svo, tm_fusion):
-    
+
+def create_kitti_seg_err_plots(tm_svo, tm_fusion, seq, segs=list(range(100, 801, 100))):
+    tm_dict = {'viso2-s': tm_svo, 'Fusion': tm_fusion}
+    vis = TrajectoryVisualizer(tm_dict)
+    fig, ax = vis.plot_segment_errors(segs)
+    output_file = 'kitti_{}_seg_err.pdf'.format(seq)
+    fig.savefig(output_file)
+
+
+
+def output_kitti_stats(tm_svo, tm_fusion, seq, segs=list(range(100, 801, 100))):
+
+
+    csv_header = ['Sequence', 'Type', 'Traj t','Traj r', 'Rel t (delta=1)', 'Rel r (delta=1)', 'Rel t (delta=5)', 'Rel r (delta=5)', 'Seg t', 'Seg r']
+
+    trans_armse_fusion_traj, rot_armse_fusion_traj = tm_fusion.mean_err(error_type='traj', rot_unit='deg')
+    trans_armse_vo_traj, rot_armse_vo_traj = tm_svo.mean_err(error_type='traj', rot_unit='deg')
+
+    trans_rel_1_fusion, rot_rel_1_fusion = tm_fusion.error_norms(error_type='rel', rot_unit='deg', delta=1)
+    trans_rel_1_vo, rot_rel_1_vo = tm_svo.error_norms(error_type='rel', rot_unit='deg', delta=1)
+
+    trans_rel_1_fusion, rot_rel_1_fusion = np.mean(trans_rel_1_fusion),  np.mean(rot_rel_1_fusion)
+    trans_rel_1_vo, rot_rel_1_vo = np.mean(trans_rel_1_vo), np.mean(rot_rel_1_vo)
+
+    trans_rel_5_fusion, rot_rel_5_fusion = tm_fusion.error_norms(error_type='rel', rot_unit='deg', delta=5)
+    trans_rel_5_vo, rot_rel_5_vo = tm_svo.error_norms(error_type='rel', rot_unit='deg', delta=5)
+
+    trans_rel_5_fusion, rot_rel_5_fusion = np.mean(trans_rel_5_fusion), np.mean(rot_rel_5_fusion)
+    trans_rel_5_vo, rot_rel_5_vo = np.mean(trans_rel_5_vo), np.mean(rot_rel_5_vo)
+
+    _, fusion_avg_errs = tm_fusion.segment_errors(segs, rot_unit='deg')
+    _, vo_avg_errs = tm_svo.segment_errors(segs, rot_unit='deg')
+
+    fus_seg_errs = [np.mean(fusion_avg_errs[:,1]), np.mean(fusion_avg_errs[:,2])]
+    vo_seg_errs = [np.mean(vo_avg_errs[:,1]), np.mean(vo_avg_errs[:,2])]
+
+    vo_stats =  [seq, 'viso2-s',
+                   trans_armse_vo_traj, rot_armse_vo_traj,
+                   trans_rel_1_vo, rot_rel_1_vo,
+                   trans_rel_5_vo, rot_rel_5_vo,
+                   vo_seg_errs[0], vo_seg_errs[1]]
+
+    fusion_stats = [seq, 'fusion',
+                  trans_armse_fusion_traj, rot_armse_fusion_traj,
+                  trans_rel_1_fusion, rot_rel_1_fusion,
+                  trans_rel_5_fusion, rot_rel_5_fusion,
+                  fus_seg_errs[0], fus_seg_errs[1]]
+
+    csv_filename = '{}_stats.csv'.format(seq)
+    with open(csv_filename, "w") as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+        writer.writerow(csv_header)
+        writer.writerow(vo_stats)
+        writer.writerow(fusion_stats)
 
 
 def main():
+
+    seqs = {'00': {'date': '2011_10_03',
+                   'drive': '0027',
+                   'frames': range(0, 4541)},
+            '01': {'date': '2011_10_03',
+                   'drive': '0042',
+                   'frames': range(0, 1101)},
+            '02': {'date': '2011_10_03',
+                   'drive': '0034',
+                   'frames': range(0, 4661)},
+            '04': {'date': '2011_09_30',
+                   'drive': '0016',
+                   'frames': range(0, 271)},
+            '05': {'date': '2011_09_30',
+                   'drive': '0018',
+                   'frames': range(0, 2761)},
+            '06': {'date': '2011_09_30',
+                   'drive': '0020',
+                   'frames': range(0, 1101)},
+            '07': {'date': '2011_09_30',
+                   'drive': '0027',
+                   'frames': range(0, 1101)},
+            '08': {'date': '2011_09_30',
+                   'drive': '0028',
+                   'frames': range(1100, 5171)},
+            '09': {'date': '2011_09_30',
+                   'drive': '0033',
+                   'frames': range(0, 1591)},
+            '10': {'date': '2011_09_30',
+                   'drive': '0034',
+                   'frames': range(0, 1201)}}
+
+    process_seqs = {'00', '02', '05'}
+    segs = list(range(100, 801, 100))
+
+    for seq in process_seqs:
+        svo_tm_path = '../kitti/svo/baseline_tm/'
+        fusion_tm_output_path = '../kitti/fusion/fusion_tms/'
+
+        fusion_file = os.path.join(fusion_tm_output_path,
+                                           'SO3_fused_single_{}_drive_{}.mat'.format(seqs[seq]['date'],
+                                                                                     seqs[seq]['drive']))
+
+        svo_file = os.path.join(svo_tm_path, '{}_drive_{}.mat'.format(seqs[seq]['date'], seqs[seq]['drive']))
+
+        tm_svo = TrajectoryMetrics.loadmat(svo_file)
+        tm_fusion = TrajectoryMetrics.loadmat(fusion_file)
+        #create_kitti_topdown_plots(tm_svo, tm_fusion, seq)
+        #output_kitti_stats(tm_svo, tm_fusion, seq, segs)
+        create_kitti_seg_err_plots(tm_svo, tm_fusion, seq, segs)
     #create_sim_world_plot()
     #create_sim_error_plot()
 
